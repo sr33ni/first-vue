d8eac45aeab4c2047cbe4d758ee2699af370ca2f
sr33ni <s4sreeni@outlook.com>
2025-11-01 09:21:22 +0530

    new commit

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 
 <template>
 <main>
-  Helloworld
+  <AboutSection />
 </main>
 
 </template>
+<script setup>
+  import AboutSection from './assets/components/AboutSection.vue'
+</script>
 
 <style scoped></style>
---
+++ src/assets/components/AboutSection.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="about-section container">
+        <h2>About Us</h2>
+        <p>
+            TechBank is a blockchain product company creating decentralized tools like TechBank,<br /> Dbank, and
+            Dwallet to simplify digital finance. We empower users with secure, transparent,<br /> and user-friendly
+            solutions for managing assets in the Web3 world.
+        </p>
+        <ul class="stats-container">
+            <li v-for="(stat, index) in stats" :key="index" class="stat-item">
+                <article>
+                    <h3 class="stat-value">{{ stat.value }}</h3>
+                    <p class="stat-label">{{ stat.label }}</p>
+                </article>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script setup>
+const stats = [
+    { value: '4', label: 'Products' },
+    { value: '2000+', label: 'Active Users' },
+    { value: '1M+', label: 'Downloads' }
+];
+</script>
