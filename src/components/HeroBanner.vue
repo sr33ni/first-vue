61809548b6f10e425a2f13a380be1e744c94360c
munnu <munaifamuhammedb@gmail.com>
2025-11-02 21:45:27 -0500

    hero banner

--- src/components/HeroBanner.vue
+++ src/components/HeroBanner.vue
@@ -1,61 +1,31 @@
 <template>
-     <section class="hero-banner">
-        <div class="hero-content container">
-            <article>
-      <img
-        src="@/assets/img/sticker.png"
-        alt="Holographic sticker"
-        class="sticker"
-      />
-      <p>
-        At TechBank, we’re pioneering the technologies of tomorrow. From blockchain
-        to AI, we’re building the digital landscape of the future, today.
-      </p>
-            </article>
-            <h1>Engineering <br>the Future</h1>
-           <div class="banner-content">
-      <p>
-        Empowering Businesses, Enabling Growth<br />
-        TechBank’s Products for Success
-      </p>
-      <button class="btn_tag" @click="viewProducts">View Products</button>
-    </div>
-        </div>
-    </section> 
-    <!-- <section class="hero-banner">
-    <div>
-      <img
-        src="@/assets/img/sticker.png"
-        alt="Holographic sticker"
-        class="sticker"
-      />
-      <p>
-        At TechBank, we’re pioneering the technologies of tomorrow. From blockchain
-        to AI, we’re building the digital landscape of the future, today.
-      </p>
-    </div>
+  <section class="hero-banner">
+    <div class="hero-content container">
+      <article>
+        <img
+          src="@/assets/img/sticker.png"
+          alt="Holographic sticker"
+          class="sticker"
+        />
+        <p>
+          At TechBank, we’re pioneering the technologies of tomorrow. From blockchain
+          to AI, we’re building the digital landscape of the future, today.
+        </p>
+      </article>
 
-     <img
-      src="@/assets/img/comp.gif"
-      alt="Hero Banner Image"
-      class="hero-image"
-    /> 
+      <h1>Engineering <br />the Future</h1>
 
-    <h1>
-      Engineering<br />
-      the Future
-    </h1>
-
-    <div class="banner-content">
-      <p>
-        Empowering Businesses, Enabling Growth<br />
-        TechBank’s Products for Success
-      </p>
-      <button class="btn_tag" @click="viewProducts">View Products</button>
+      <div class="banner-content">
+        <p>
+          Empowering Businesses, Enabling Growth<br />
+          TechBank’s Products for Success
+        </p>
+        <button class="btn_tag" @click="viewProducts">View Products</button>
+      </div>
     </div>
-  </section> -->
-   <p class="explore-link">+ SCROLL TO EXPLORE +</p>
+    <p class="explore-link">+ SCROLL TO EXPLORE +</p>
+  </section>
 </template>
-  
+
 
 
